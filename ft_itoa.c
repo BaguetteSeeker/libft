41a2ca5620a5a8913d20c2bc84b4ae29c423a301
Enzo Pinaud <epinaud@bess-f2r4s6.clusters.42paris.fr>
2024-06-03 21:12:05 +0200

    Itoa done & passed test

---
+++ ft_itoa.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 18:05:27 by epinaud           #+#    #+#             */
+/*   Updated: 2024/06/03 21:10:22 by epinaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	count_digits(long int n)
+{
+	size_t	count;
+
+	count = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+	{
+		n = -n;
+		count++;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	long int	lnum;
+	size_t		count;
+	char		*str;
+
+	lnum = n;
+	count = count_digits(n);
+	str = malloc((count + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	str[count--] = '\0';
+	if (lnum == 0)
+		str[0] = '0';
+	if (lnum < 0)
+	{
+		str[0] = '-';
+		lnum = -lnum;
+	}
+	while (lnum > 0)
+	{
+		str[count] = lnum % 10 + '0';
+		count--;
+		lnum /= 10;
+	}
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 19:06:08 by epinaud           #+#    #+#             */
-/*   Updated: 2024/05/29 21:19:19 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/06/02 19:08:39 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,5 @@ char    *ft_substr(char const *s, unsigned int start, size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 #endif
