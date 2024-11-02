7ce8a188489c7588c7a121a8c2f4ea373f7a2fa1
epinaud <enzo.pinaud+42@protonmail.com>
2024-11-02 15:37:55 +0100

    Fixed invalid free on top ptr;

--- ft_clean_memtree.c
+++ ft_clean_memtree.c
@@ -6,7 +6,7 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 17:19:36 by epinaud           #+#    #+#             */
-/*   Updated: 2024/10/31 18:13:28 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/11/02 15:27:58 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 char	**ft_clean_memtree(char **ptr)
 {
+	char	**dptr_strt;
+
 	if (!ptr)
 		return (NULL);
+	dptr_strt = ptr;
 	while (*ptr)
 		free(*ptr++);
-	free(ptr);
+	free(dptr_strt);
 	return (NULL);
 }
