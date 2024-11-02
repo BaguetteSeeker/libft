fa76141a21e45689153b7841d0beded35897fe27
epinaud <enzo.pinaud+42@protonmail.com>
2024-11-02 15:35:54 +0100

    Realloc : stable but still in testing stage;

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,25 +6,43 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 01:17:35 by epinaud           #+#    #+#             */
-/*   Updated: 2024/10/31 18:18:16 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/11/02 15:32:33 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+void	put_addrlst(char **ptr)
+{
+	int	i;
+
+	printf("Main ptr addr is %p —\n", ptr);
+	i = 0;
+	while (ptr[i])
+	{
+		printf("Row %d addr is %p\n", i, ptr[i]);
+		i++;
+	}
+}
 
 void	*ft_realloc(void *ptr, size_t newsize)
 {
-	char	*newptr;
+	void	*newptr;
 	size_t	cursize;
-	ft_printf("Entered realloc \n");
-	if (!ptr)
+
+	if (!newsize && ptr)
+        return (free(ptr), NULL);
+	else if (!ptr)
 		return (malloc(newsize));
-	cursize = sizeof(ptr);
-	ft_printf("Curr double ptr is %ld siz\n", cursize);
+	cursize = (ft_ptrlen(ptr) + 1) * 8;
+	printf("Currsiz : sizof oldptr[0] is %ld\n Bytes in old ptr : %ld\n", sizeof(ptr[0]), ft_ptrlen(ptr));
+	printf("Old double ptr has %ld subptrs\n", cursize - 1);
 	if (newsize <= cursize)
 		return (ptr);
 	newptr = malloc(newsize);
-	ft_memcpy(ptr, newptr, cursize);
+	printf("Just realloced a new ptr %p of siz %ld\n", newptr, newsize);
+	ft_memcpy(newptr, ptr, cursize);
 	free(ptr);
 	return (newptr);
 }
