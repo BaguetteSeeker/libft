7d4cc84e753ab4328af7005465cc488994053c77
epinaud <enzo.pinaud+42@protonmail.com>
2024-11-12 19:14:09 +0100

    Static*[fd] is no longer malloced, it is now an array of siz buffsiz : static[fd][buffsiz+1]; When GNL finds the EOF as the first char of the line, it frees the buffer and returns NULL (previously returned buffer of size 1 with null char as content, causing still reachable issues in upper lvl uses);

--- get_next_line.c
+++ get_next_line.c
@@ -6,27 +6,27 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 20:00:18 by epinaud           #+#    #+#             */
-/*   Updated: 2024/10/04 02:43:29 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/11/12 19:07:21 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	read_errchk(int read_return)
+static int	read_errchk(int bytes_read)
 {
 	if (errno == EAGAIN || errno == EWOULDBLOCK)
 		return (0);
 	else if (errno == EBADF)
 		return (0);
-	else if (read_return > 0)
-		return (read_return);
+	else if (bytes_read > 0)
+		return (bytes_read);
 	else if (errno == EINTR)
 		return (0);
 	else if (errno == EIO)
 		return (0);
-	else if (read_return == 0)
+	else if (bytes_read == 0)
 		return (0);
-	else if (read_return == -1)
+	else if (bytes_read == -1)
 		return (0);
 	else
 		return (0);
@@ -42,7 +42,7 @@ static char	*fetch_file_content(int fd)
 	if (!buffer)
 		return (NULL);
 	bytes_read = read(fd, buffer, GNL_BUFFSIZ);
-	if (!read_errchk(bytes_read))
+	if (!read_errchk(bytes_read) || buffer[0] == '\0')
 		return (free(buffer), NULL);
 	return (buffer);
 }
@@ -58,45 +58,46 @@ static char	*expand_current_line(char **currline, int fd)
 	lntmp = *currline;
 	*currline = ft_strjoin(*currline, next_read);
 	free(next_read);
-	free(lntmp);
+	if (lntmp)
+		free(lntmp);
 	return (*currline);
 }
 
-static char	*cut_nxt_nl(char **currline, char **stash, char *eol)
+static char	*cut_nxt_nl(char **currln, char *remains, char *eol_curr)
 {
 	char	*tmptr;
 
-	*stash = ft_substr(*currline, eol - *currline + 1, ft_strlen(eol + 1));
-	tmptr = ft_substr(*currline, 0, eol - *currline + 1);
-	free(*currline);
-	*currline = tmptr;
-	return (*currline);
+	ft_bzero(remains, ft_strlen(remains));
+	memmove(remains, eol_curr + 1, ft_strlen(eol_curr + 1));
+	tmptr = ft_substr(*currln, 0, eol_curr - *currln + 1);
+	free(*currln);
+	*currln = tmptr;
+	return (*currln);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*nextline[1025];
-	char		*currline;
+	static char	remains[1024 + 1][GNL_BUFFSIZ + 1] = {0};
+	char		*line;
 	char		*eol;
 
 	if (fd < 0 || fd > 1024 || GNL_BUFFSIZ < 1 || read(fd, 0, 0))
-		return (free(nextline[fd]), nextline[fd] = NULL, NULL);
-	if (!nextline[fd])
-		currline = fetch_file_content(fd);
+		return (NULL);
+	if (!remains[fd][0])
+		line = fetch_file_content(fd);
 	else
-		currline = nextline[fd];
-	while (currline)
+		line = ft_strdup(remains[fd]);
+	while (line)
 	{
-		eol = ft_strchr(currline, '\n');
+		eol = ft_strchr(line, '\n');
 		if (eol)
 		{
 			if (eol[1])
-				return (cut_nxt_nl(&currline, &nextline[fd], eol));
-			return (nextline[fd] = NULL, currline);
+				return (cut_nxt_nl(&line, remains[fd], eol));
+			return (ft_bzero(remains[fd], ft_strlen(remains[fd])), line);
 		}
-		if (!expand_current_line(&currline, fd))
-			if (currline)
-				return (nextline[fd] = NULL, currline);
+		if (!expand_current_line(&line, fd) && line)
+			return (ft_bzero(remains[fd], ft_strlen(remains[fd])), line);
 	}
-	return (currline);
+	return (line);
 }
