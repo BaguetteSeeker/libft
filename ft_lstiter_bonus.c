dc297258953e3cdb6eeb4d6c2db47d6df452bdc6
epinaud <enzo.pinaud+42@protonmail.com>
2024-11-16 18:24:55 +0100

    Patched wrongly ordered call between the mapped function and the recursive call from iter to itself;

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 00:58:00 by epinaud           #+#    #+#             */
-/*   Updated: 2024/11/05 12:51:04 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/11/16 18:23:41 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_lstiter(LL_TYP *lst, void (*f)(LL_TYP *))
 		return ;
 	if (lst)
 	{
-		ft_lstiter(lst->next, f);
 		f(lst);
+		ft_lstiter(lst->next, f);
 	}
 }
