916b16c343622536437f4b2ef5280613a5995948
epinaud <enzo.pinaud+42@protonmail.com>
2025-05-12 19:02:43 +0200

    Patched inverse logical gate bug introduced when lstclear and lstdelone were changed to become non-blocking in case of the absence of a provided del() handler

--- src/ft_lstdelone_bonus.c
+++ src/ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 23:49:59 by epinaud           #+#    #+#             */
-/*   Updated: 2025/05/12 11:55:48 by epinaud          ###   ########.fr       */
+/*   Updated: 2025/05/12 18:58:04 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstdelone(LL_TYP *lst, void (*del)(LL_TYP *lst))
 {
 	if (lst)
 	{
-		if (!del)
+		if (del)
 			del(lst);
 		free(lst);
 	}
