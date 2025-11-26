8515813f9616669b073cb1c00bd7993435f4f65d
epinaud <enzo.pinaud+42@protonmail.com>
2025-11-26 01:27:33 +0100

    Atof now properly handles sign conversion: it prevents multiple sign conversion conflicts by using a dedicated unsigned version of atoi2, ensuring sign conversion is only ever done once in atof itself

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 18:45:30 by epinaud           #+#    #+#             */
-/*   Updated: 2025/10/22 20:56:46 by epinaud          ###   ########.fr       */
+/*   Updated: 2025/11/26 00:47:26 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,28 @@ int	ft_atoi2(const char *nptr, long *sum)
 	return (*sum *= sign, i);
 }
 
+//Similar to atoi2, but doesn't apply sign
+//Solely for internal atof() use
+static int	ft_atoi_unsigned(const char *nptr, long *sum)
+{
+	size_t	i;
+
+	i = 0;
+	while (nptr[i] == ' ' || nptr[i] == '\t' || nptr[i] == '\n'
+		|| nptr[i] == '\v' || nptr[i] == '\f' || nptr[i] == '\r')
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+		i++;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		if (__INT_MAX__ / 10 < *sum || *sum * 10 > INT_MAX - (nptr[i] - 48))
+			return (-1);
+		*sum = *sum * 10 + nptr[i] - 48;
+		i++;
+	}
+	return (i);
+}
+
 static long	ft_powf(size_t pow, long n)
 {
 	long	result;
@@ -81,18 +103,23 @@ int	ft_atof(const char *nptr, double *sum)
 	int		mantln;
 	long	exponent;
 	long	mantissa;
+	int		sign;
 
+	if (*nptr == '-')
+		sign = -1;
+	else
+		sign = 1;
 	exponent = 0;
 	mantissa = 0;
-	exptln = ft_atoi2(nptr, &exponent);
+	exptln = ft_atoi_unsigned(nptr, &exponent);
 	*sum += exponent;
 	if (exptln < 1 || nptr[exptln] != '.')
 		return (exptln);
-	mantln = ft_atoi2(&nptr[exptln + 1], &mantissa);
+	mantln = ft_atoi_unsigned(&nptr[exptln + 1], &mantissa);
 	if (mantln == -1)
 		return (-1);
 	else if (mantln == 0)
 		return (exptln + mantln);
-	return (*sum += ((double)mantissa
-			/ ft_powf(10, ft_nbrdig(mantissa))), exptln + mantln + 1);
+	*sum += (float)mantissa / ft_powf(10, ft_nbrdig(mantissa));
+	return (*sum *= sign, exptln + mantln + 1);
 }
