020a4015fe407abf4772301b9232b6eaa83f2060
epinaud <enzo.pinaud+42@protonmail.com>
2025-11-28 20:15:36 +0100

    Reverted atof changes regarding leading zeroes in the decimal

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 18:45:30 by epinaud           #+#    #+#             */
-/*   Updated: 2025/11/28 02:41:15 by epinaud          ###   ########.fr       */
+/*   Updated: 2025/11/28 20:12:52 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,22 +97,6 @@ static long	ft_powf(size_t pow, long n)
 	return (result);
 }
 
-// float	push_zeroes(long mantissa, size_t mantln)
-// {
-// 	double	number;
-// 	size_t	diff;
-
-// 	diff = mantln - nbrdig(mantissa);
-
-// 	while (diff != 0)
-// 	{
-// 		mantissa /= 10;
-// 		diff--;
-// 	}
-// 	return ();
-
-// }
-
 int	ft_atof(const char *nptr, double *sum)
 {
 	int		exptln;
@@ -136,6 +120,6 @@ int	ft_atof(const char *nptr, double *sum)
 		return (-1);
 	else if (mantln == 0)
 		return (exptln + mantln);
-	*sum += (float)mantissa / ft_powf(10, ft_strlen(&nptr[exptln + 1]));
+	*sum += (double)mantissa / ft_powf(10, mantln);
 	return (*sum *= sign, exptln + mantln + 1);
 }
